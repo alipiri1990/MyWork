ae7657e10644ff883a0d301e2a93dcfefe3931f6
alipiri1990 <alipiri19900@gmail.com>
2021-10-05 13:20:38 +0330

    add item

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //hello world
+
+        Log.d("AAA","AAA");
+        Log.d("BBB","BBB");
+        Log.d("CCC","CCC");
     }
 }
