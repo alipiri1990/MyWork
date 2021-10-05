7bb935b31bd3dde947667326ba2ad73c5d79e6b0
alipiri1990 <alipiri19900@gmail.com>
2021-10-05 14:28:07 +0330

    update

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -3,19 +3,12 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //hello world
-
-        Log.d("WWW","WWW");
-        Log.d("AAA","AAA");
-
+        setContentView(R.layout.activity_login);
     }
 }
