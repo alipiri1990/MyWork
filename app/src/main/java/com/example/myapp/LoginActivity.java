aa180d18ebce48f6a529b5a34512c7e31b31b37e
alipiri1990 <alipiri19900@gmail.com>
2021-10-05 14:45:56 +0330

    my

--- app/src/main/java/com/example/myapp/LoginActivity.java
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -10,7 +10,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //yyy
-        //uuu
+        //rrr
     }
 }
